4128068192ad8f423cbc93473e566a0841f1bd26
Kamil Kordecki <kkordecky@gmail.com>
2022-12-28 00:53:15 +0100

    Add Author model

--- src/authors/models.py
+++ src/authors/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Author(models.Model):
+    name    = models.CharField(max_length=50)
+    is_fav  = models.BooleanField(default=False)
+
+    def __str__(self) -> str:
+        """Returns Author's name."""
+        return self.name
+    
+    
