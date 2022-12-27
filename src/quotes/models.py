91b4d6bfdbac7b0b60ee8448d724fe2fdbe32f1b
Kamil Kordecki <kkordecky@gmail.com>
2022-12-28 00:53:25 +0100

    Add Quote model

--- src/quotes/models.py
+++ src/quotes/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from src.authors.models import Author
 
-# Create your models here.
+class Quote(models.Model):
+    title       = models.CharField(null=True, blank=True,  max_length=150)
+    content     = models.TextField()
+    author      = models.ForeignKey(Author, on_delete=models.CASCADE)
+    is_fav      = models.BooleanField(default=False)
+    pub_date    = models.DateField('date published')
+
+
+    def __str__(self) -> str:
+        """Returns [Quote's Title] | by [Author's name]. """
+
+        if self.title == None or self.title == "":
+            return "[No Title] | by " + str(self.author)
+
+        return self.title + " | by " + str(self.author)
+
