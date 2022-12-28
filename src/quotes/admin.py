6155a827cee557fed8f1d548562bb2f4ec92ac37
Kamil Kordecki <kkordecky@gmail.com>
2022-12-28 01:53:49 +0100

    Add Source model

--- src/quotes/admin.py
+++ src/quotes/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import (Author, Quote)
+from .models import (Author, Quote, Source)
 
 # Register your models here.
 
 admin.site.register(Author)
 admin.site.register(Quote)
+admin.site.register(Source)
--- src/quotes/models.py
+++ src/quotes/models.py
@@ -1,17 +1,25 @@
 from django.db import models
 
 class Author(models.Model):
-    name    = models.CharField(max_length=50)
+    name    = models.CharField(max_length=40)
     is_fav  = models.BooleanField(default=False)
 
     def __str__(self) -> str:
         """Returns Author's name."""
         return self.name
 
+class Source(models.Model):
+    source  = models.CharField(max_length=80)
+
+    def __str__(self) -> str:
+        """Returns source of Quote"""
+        return self.source
+
 class Quote(models.Model):
     title       = models.CharField(null=True, blank=True,  max_length=150)
     content     = models.TextField()
     author      = models.ForeignKey(Author, on_delete=models.CASCADE)
+    source      = models.ForeignKey(Source, on_delete=models.CASCADE, null=True, blank=True)
     is_fav      = models.BooleanField(default=False)
     pub_date    = models.DateField('date published')
 
