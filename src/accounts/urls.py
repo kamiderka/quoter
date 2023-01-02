e50d6fe07d6681d86e87c0947be5805ecef50bda
Kamil Kordecki <kkordecky@gmail.com>
2023-01-02 21:36:27 +0100

    Add basic User registering functionality

---
+++ src/accounts/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import ( RegisterView )
+
+app_name = 'accountss'
+urlpatterns = [
+    path('register', RegisterView.as_view(), name='register') #
+
+
+    
+]
--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.views import generic
 
 # Create your views here.
+
+class RegisterView(generic.FormView):
+    template_name = 'register.html'
+    form_class = UserCreationForm
+    success_url = '/'
+
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
+ 
+        
