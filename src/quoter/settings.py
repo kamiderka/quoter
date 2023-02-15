7328c7f03c8786171076e97c09bf08c390e614fc
Kamil Kordecki <kkordecky@gmail.com>
2023-02-15 20:35:26 +0100

    commit

--- src/quoter/settings.py
+++ src/quoter/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'RENDER' not in os.environ
+DEBUG = True #'RENDER' not in os.environ
 
 ALLOWED_HOSTS = []
 
