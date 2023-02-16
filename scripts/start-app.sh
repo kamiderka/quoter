7c87be028971c7d29af2382949c13f8f21698e66
Kamil Kordecki <kkordecky@gmail.com>
2023-02-16 09:24:01 +0100

    Simplify scripts

--- scripts/start-app.sh
+++ scripts/start-app.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-if [[ $VIRTUAL_ENV == "" ]]; then
-    echo "Virtual environment is not active. Activate the virtual environment."
-    exit 1
-else
-    echo "Virtual environment activated."
-fi
-
 if [[ -f ../requirements.txt ]]; then
     echo "Installing required packages from requirements.txt..."
     pip install -r ../requirements.txt
